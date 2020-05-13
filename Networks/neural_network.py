cleaning and fixing order data and fixing backprop also created Layer class

Networks/neural_network.py
@@ -1,9 +1,9 @@
 from network_model import NetworkModel
 from typing import List
-from activation import Activation, Sigmoid, Tanh
+from activation import Activation, Sigmoid, Tanh, Linear
 from losses import Loss, MSE
 import numpy as np
-
+from layer import Dense
 
 class NeuralNetwork(NetworkModel):
     def __init__(self, layers_num: List[int], activations: List[Activation], loss_func: Loss, lr: float, min_init_weight=-0.5, max_init_weight=0.5):
@@ -19,45 +19,37 @@ class NeuralNetwork(NetworkModel):
 
     def predict(self, data: np.ndarray):
         for i in range(len(self.layer_weights)):
-            data = data.dot(self.layer_weights[i]) + self.biases[i]
-            data = self.activations[i].calculate(data)
+            data = Dense.feed_forward(data, self.layer_weights[i], self.biases[i], self.activations[i])
         return data
 
     def train_vanish_grad(self, my_input: np.ndarray, labels: np.ndarray):
         data = my_input
-        layer_output = []
+        layer_outputs = []
         for i in range(len(self.layer_weights)):
-            layer_output.append(data)
-            data = data.dot(self.layer_weights[i]) + self.biases[i]
-            data = self.activations[i].calculate(data)
-        # data is output now
+            layer_outputs.append(data)
+            data = Dense.feed_forward(data, self.layer_weights[i], self.biases[i], self.activations[i])
 
-        error_deriv = self.loss_function.derivative(data, labels)
-        deltas = [self.activations[-1].derivative(data) * error_deriv]
-        for i in range(len(self.layer_weights) - 1, 0, -1):
-            deltas.append(self.activations[i].derivative(layer_output[i]) * np.dot(deltas[-1], self.layer_weights[i].T))
-
-        deltas.reverse()
-        for i in range(len(deltas)):
-            self.layer_weights[i] = self.layer_weights[i] - self.lr * np.sign(np.dot(layer_output[i].T, deltas[i]))
-            self.biases[i] = self.biases[i] - self.lr * np.sign(np.sum(deltas[i], axis=0))
+        for i in range(len(self.layer_weights), 0, -1):
+            if i == len(self.layer_weights):
+                delta = Dense.back_propagate_last_layer(data, labels, self.layer_weights[i - 1], self.biases[i - 1],
+                                                        layer_outputs[i - 1], self.loss_function, self.activations[-1],
+                                                        self.lr, vanish_grad=True)
+            else:
+                delta = Dense.back_propagate(delta, layer_outputs[i], self.layer_weights[i], self.layer_weights[i - 1],
+                                             self.biases[i - 1], layer_outputs[i - 1], self.activations[i - 1], self.lr, vanish_grad=True)
 
     def train(self, my_input: np.ndarray, labels: np.ndarray):
         data = my_input
         layer_outputs = []
         for i in range(len(self.layer_weights)):
             layer_outputs.append(data)
-            data = self.activations[i].calculate(np.dot(data, self.layer_weights[i]) + self.biases[i])
+            data = Dense.feed_forward(data, self.layer_weights[i], self.biases[i], self.activations[i])
 
-        error_deriv = self.loss_function.derivative(data, labels)
-        deltas = [self.activations[-1].derivative(data) * error_deriv]
-        for i in range(len(self.layer_weights) - 1, 0, -1):
-            deltas.append(self.activations[i].derivative(layer_outputs[i]) * np.dot(deltas[-1], self.layer_weights[i].T))
-
-        deltas.reverse()
-        for i in range(len(deltas)):
-            self.layer_weights[i] = self.layer_weights[i] - self.lr * np.dot(layer_outputs[i].T, deltas[i])
-            self.biases[i] = self.biases[i] - self.lr * np.sum(deltas[i], axis=0)
+        for i in range(len(self.layer_weights), 0, -1):
+            if i == len(self.layer_weights):
+                delta = Dense.back_propagate_last_layer(data, labels, self.layer_weights[i-1], self.biases[i-1], layer_outputs[i-1], self.loss_function, self.activations[-1], self.lr)
+            else:
+                delta = Dense.back_propagate(delta, layer_outputs[i], self.layer_weights[i], self.layer_weights[i-1], self.biases[i-1], layer_outputs[i-1], self.activations[i-1], self.lr)
 
     def save_weights(self, path: str, weights_file: str, bias_file: str):
         np.save(path + "/" + weights_file, self.layer_weights, allow_pickle=True)
@@ -69,7 +61,7 @@ class NeuralNetwork(NetworkModel):
 
 
 def main():
-    nn = NeuralNetwork([2, 4, 1], [Tanh, Tanh], MSE, 1)
+    nn = NeuralNetwork([2, 4, 1], [Tanh, Tanh], MSE, 0.1)
     data = np.array([
         [0, 0],
         [1, 0],
@@ -78,11 +70,10 @@ def main():
     ])
     labels = np.array([[-1, 1, 1, -1]]).T
     print(nn.predict(data))
-    for i in range(10000):
+    for i in range(20000):
         nn.train(data, labels)
     print(nn.predict(data))
 
 
 if __name__ == "__main__":
     main()
-

Projects/DDQN/MineSweeper/MineSweeper.py
@@ -10,37 +10,9 @@ import datetime
 from statistics import mean
 import pickle as pkl
 import glob
-
+import os
 
 wins = 0
-
-
-def play_game(env: Env, train_net: DDQN, target_net: DDQN, epsilon: float, copy_step: int, wins: int, is_legal_move=None, info=lambda: None):
-    rewards = 0
-    iter = 0
-    done = False
-    observations = env.reset()
-    losses = list()
-    while not done:
-        if is_legal_move is None:
-            action = train_net.get_action(observations, epsilon)
-        else:
-            action = train_net.get_legal_action(observations, epsilon, is_legal_move)
-        prev_observations = observations
-        observations, reward, done, did_win = env.step(action)
-        if did_win:
-            wins += 1
-        rewards += reward
-        if done:
-            env.reset()
-        exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done}
-        train_net.add_experience(exp)
-        loss = train_net.train(target_net)
-        losses.append(loss)
-        iter += 1
-        if iter % copy_step == 0:
-            target_net.copy_weights(train_net)
-    return rewards, mean(losses), wins, observations
 
 
 def main():
@@ -48,25 +20,33 @@ def main():
     host = "127.0.0.1"
     port = 2000
     env = SocketEnv(host, port)
-    folder_name = "sigmoid_gam001"
+    folder_name = "lr_01_batch_700"
     saving_path = "saved_data/" + folder_name
+    dir_num = len([d for d in os.listdir(saving_path) if os.path.isdir(saving_path + "/" + d)])
+    take_from = saving_path + "/" + str(dir_num-1)
+    os.mkdir(saving_path + "/" + str(dir_num))
+    saving_path = saving_path + "/" + str(dir_num)
     gamma = 0.01
     copy_step = 10
     loss_function = MSE
     layers = [env.input_num(), 64, env.action_num()]
-    max_experiences = 10000
-    min_experiences = 100
-    batch_size = 32
-    lr = 0.05
+    max_experiences = 20000
+    min_experiences = 1000
+    batch_size = 700
+    lr = 0.1
     calculation_step = 1000
     monitoring_step = 200
-    runs_number = int(5e6)
-    train_from_start = True
+    runs_number = int(1e6)
+    train_from_start = False
     estimate_time = True
 
-    train_net = DDQN(NeuralNetwork(layers, [Sigmoid, Linear], loss_function, lr), layers,  loss_function, gamma, max_experiences, min_experiences, batch_size)
+    train_net = DDQN(NeuralNetwork(layers, [Tanh, Linear], loss_function, lr), layers,  loss_function, gamma, max_experiences, min_experiences, batch_size)
+
+    if dir_num == 0:
+        train_from_start = True
+
     if not train_from_start:
-        train_net.model.load_weights(saving_path, "weights.npy", "biases.npy")
+        train_net.model.load_weights(take_from, "weights.npy", "biases.npy")
     target_net = copy.deepcopy(train_net)
 
     episode_list = []
@@ -81,7 +61,7 @@ def main():
     if train_from_start:
         epsilon = 1
     else:
-        with open(saving_path + "/epsilon.txt", "r") as f:
+        with open(take_from + "/epsilon.txt", "r") as f:
             data = f.read()
             epsilon = float(data)
     decay = 0.9999
@@ -112,6 +92,9 @@ def main():
                     f.write(str(epsilon))
             if n % monitoring_step == 0:
                 if not estimate_time:
+                    avg_rewards = total_rewards[max(0, n - calculation_step):(n + 1)].mean()
+                    avg_losses = total_losses[max(0, n - calculation_step):(n + 1)].mean()
+                    avg_reveal_percents = total_reveal_percent[max(0, n - calculation_step):(n + 1)].mean()
                     print("episode:", n, "episode reward:", total_reward, "eps:", epsilon, "avg reward (last",
                           str(calculation_step) + "):", avg_rewards,
                           "avg loss (last", str(calculation_step) + "):", avg_losses, "last", calculation_step,
@@ -122,42 +105,80 @@ def main():
     except:
         pass
 
+    finally:
+        avg_rewards = total_rewards[max(0, n - calculation_step):(n + 1)].mean()
+        avg_losses = total_losses[max(0, n - calculation_step):(n + 1)].mean()
+        avg_reveal_percents = total_reveal_percent[max(0, n - calculation_step):(n + 1)].mean()
+        win_list.append(wins)
+        reveal_list.append(avg_reveal_percents)
+        episode_list.append(n)
+        avg_rwd_list.append(avg_rewards)
+        losses_list.append(avg_losses)
 
-    print("avg reward for last", calculation_step, "episodes:", avg_rewards)
-    print("total win percent", (np.sum(np.array(win_list)))/n)
+        print("avg reward for last", calculation_step, "episodes:", avg_rewards)
+        print("total win percent", (np.sum(np.array(win_list)))/n)
 
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-    ax1.plot(episode_list, avg_rwd_list)
-    ax1.set(xlabel="episode", ylabel="avg reward last " + str(calculation_step))
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+        ax1.plot(episode_list, avg_rwd_list)
+        ax1.set(xlabel="episode", ylabel="avg reward last " + str(calculation_step))
 
-    ax2.plot(episode_list, losses_list)
-    ax2.set(xlabel="episode", ylabel="avg loss last" + str(calculation_step))
+        ax2.plot(episode_list, losses_list)
+        ax2.set(xlabel="episode", ylabel="avg loss last" + str(calculation_step))
 
-    ax3.plot(episode_list, win_list)
-    ax3.set(xlabel="episode", ylabel="wins last" + str(calculation_step))
+        ax3.plot(episode_list, win_list)
+        ax3.set(xlabel="episode", ylabel="wins last" + str(calculation_step))
 
-    ax4.plot(episode_list, reveal_list)
-    ax4.set(xlabel="episode", ylabel="avg percent board last" + str(calculation_step))
+        ax4.plot(episode_list, reveal_list)
+        ax4.set(xlabel="episode", ylabel="avg percent board last" + str(calculation_step))
 
-    target_net.model.save_weights(saving_path, "weights.npy", "biases.npy")
+        target_net.model.save_weights(saving_path, "weights.npy", "biases.npy")
 
-    with open(saving_path + "/epsilon.txt", "w") as f:
-        f.write(str(epsilon))
+        with open(saving_path + "/epsilon.txt", "w") as f:
+            f.write(str(epsilon))
 
-    with open(saving_path + "/rewards.txt", "w") as f:
-        f.write(str(avg_rwd_list))
+        with open(saving_path + "/rewards.txt", "w") as f:
+            f.write(str(avg_rwd_list))
 
-    with open(saving_path + "/losses.txt", "w") as f:
-        f.write(str(losses_list))
+        with open(saving_path + "/losses.txt", "w") as f:
+            f.write(str(losses_list))
 
-    with open(saving_path + "/wins.txt", "w") as f:
-        f.write(str(win_list))
+        with open(saving_path + "/wins.txt", "w") as f:
+            f.write(str(win_list))
 
-    with open(saving_path + "/reveals.txt", "w") as f:
-        f.write(str(reveal_list))
+        with open(saving_path + "/reveals.txt", "w") as f:
+            f.write(str(reveal_list))
 
-    pkl.dump(fig, open(saving_path + "/plot" + str(len(glob.glob1("saving_path", "*.p"))) + ".p", 'wb'))
-    plt.show()
+        pkl.dump(fig, open(saving_path + "/plot.p", 'wb'))
+        plt.show()
+
+
+def play_game(env: Env, train_net: DDQN, target_net: DDQN, epsilon: float, copy_step: int, wins: int, is_legal_move=None, info=lambda: None):
+    rewards = 0
+    iter = 0
+    done = False
+    observations = env.reset()
+    losses = list()
+    while not done:
+        if is_legal_move is None:
+            action = train_net.get_action(observations, epsilon)
+        else:
+            action = train_net.get_legal_action(observations, epsilon, is_legal_move)
+        prev_observations = observations
+        observations, reward, done, did_win = env.step(action)
+        if did_win:
+            wins += 1
+        rewards += reward
+        if done:
+            env.reset()
+        exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done}
+        train_net.add_experience(exp)
+        loss = train_net.train(target_net)
+        losses.append(loss)
+        iter += 1
+        if iter % copy_step == 0:
+            target_net.model.layer_weights = np.copy(train_net.model.layer_weights)
+            target_net.model.biases = np.copy(train_net.model.biases)
+    return rewards, mean(losses), wins, observations
 
 
 def board_percent_revealed(board: np.ndarray):